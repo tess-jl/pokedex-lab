68c1434892e21266fefcc8256ca7dc29dd21a3a2
Tess Lameyer <tjlameyer@gmail.com>
2019-10-31 12:59:51 -0700

    commented out test files for now

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,16 @@
-// IMPORT MODULES under test here:
-// import example from '../src/example.js';
+// // IMPORT MODULES under test here:
+// // import example from '../src/example.js';
 
-const test = QUnit.test;
+// const test = QUnit.test;
 
-test('time to test a function', function(assert) {
-    //Arrange
-    // Set up your parameters and expectations
+// test('time to test a function', function(assert) {
+//     //Arrange
+//     // Set up your parameters and expectations
 
-    //Act 
-    // Call the function you're testing and set the result to a const
+//     //Act 
+//     // Call the function you're testing and set the result to a const
 
-    //Assert
-    // Make assertions about what is expected valid result
-    assert.equal(true, false);
-});
+//     //Assert
+//     // Make assertions about what is expected valid result
+//     assert.equal(true, false);
+// });
